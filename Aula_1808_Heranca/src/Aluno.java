0aeca58abe0e1cd293272c3777840315b80c7fd3
jheykocarreno <jheyko1000@gmail.com>
2025-08-18 21:53:32 -0300

    1508

---
+++ Aula_1808_Heranca/src/Aluno.java
@@ -0,0 +1,11 @@
+public class Aluno {
+    protected String nome;
+
+    public Aluno(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+}
---
+++ Aula_1808_Heranca/src/Turma.java
@@ -0,0 +1,13 @@
+public class Turma extends Aluno{
+
+    String nomeDaTurma;
+    int indice;
+    Aluno[] alunos = new Aluno[indice];
+
+    public Turma(String nomeDaTurma, int capacidade){
+        this.nomeDaTurma = nomeDaTurma;
+        this.indice = capacidade;
+    }
+
+    public 
+}
