891aa569108ab5bfe02058554d72aedd0dbba7c7
jheykocarreno <jheyko1000@gmail.com>
2025-08-27 21:37:38 -0300

    DDL Ecommerce

--- Projeto_ECommerce/Banco de dados/DDL Clinica.sql
+++ Projeto_ECommerce/Banco de dados/DDL Clinica.sql
@@ -3,28 +3,28 @@
 */
 
 --DDL - Criar - Create (Schema, Tabela)
-create schema clinica
+create schema clinica;
 
 create table clinica.medico(
 	id_medico INT primary key generated always as identity, 
 	nome TEXT not null,
 	crm TEXT not null,
 	especialidade text not null
-)
+);
 
 create table clinica.paciente(
 	id_paciente int primary key generated always as identity,
 	nome text not null,
 	idade int not null, 
 	data_nascimento date not null
-)
+);
 
 create table clinica.clinica(
 	id_clinica int primary key generated always as identity,
 	nome text not null, 
 	descricao text not null,
 	endereco text
-)
+);
 
 create table clinica.consulta(
 	id_consulta int primary key generated always as identity,
@@ -37,8 +37,30 @@ create table clinica.consulta(
 	--maneira abreviada
 	id_clinica int not null references clinica.clinica(id_clinica),
 	id_paciente int not null references clinica.paciente(id_paciente)
-)
+);
+
+--ALTER - alterar tabela
+ALTER TABLE clinica.paciente
+add column cpf varchar(14) unique;
+--unique - impede que seja cadastrado
+-- algo que ja existe no banco.
+
+--Apagar coluna
+ALTER TABLE clinica.paciente
+DROP COLUMN cpf;
+
+--Renomear tabela
+ALTER TABLE clinica.paciente
+RENAME TO novopaciente
+
+-- TRUNCATE - Limpa a tabela
+--TRUNCATE TABLE clinica.consulta;
+--RESTART IDENTITY; --reinicia a sequencia
 
 --Apagar tabela ou schema - DROP
+/*
 drop table clinica.consulta;
 drop table clinica.clinica;
+drop table clinica.medico;
+drop table clinica.paciente;
+*/
---
+++ Projeto_ECommerce/Banco de dados/DDL Ecommerce.sql
@@ -0,0 +1,42 @@
+CREATE SCHEMA ecommerce;
+
+CREATE TABLE ecommerce.cliente(
+	id_cliente INT primary key generated always as identity, 
+	nome TEXT not null,
+	email TEXT not null,
+	senha TEXT not null,
+	telefone VARCHAR(11),
+	especialidade text not null
+);
+
+CREATE TABLE ecommerce.pedido(
+	id_pedido INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id_cliente INT not null references ecommerce.cliente(id_cliente),
+	data_pedido DATE not null,
+	valor_total NUMERIC(10,4) not null, 
+	status TEXT not null
+);
+
+CREATE TABLE ecommerce.produto(
+	id_produto INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	nome_produto text not null, 
+	descricao text not null, 
+	preco NUMERIC(10,4) not null, 
+	estoque_disponivel INT not null, 
+	imagem_url TEXT
+);
+
+CREATE TABLE ecommerce.item_pedido(
+	id_item INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id_pedido INT not null references ecommerce.pedido(id_pedido),
+	id_produto INT not null referencies ecommerce.produto(id_produto),
+	quantidade INT not null
+);
+
+CREATE TABLE ecommerce.pagamento(
+	id_pagamento INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id_pedido INT not null references ecommerce.pedido(id_pedido),
+	forma_pagamento TEXT not null, 
+	status text not null, 
+	data_pagamento TIMESTAMPTZ not null
+);
